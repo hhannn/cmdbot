17668acd3a355b0aaa9cfb3611075c2366d6f3ea
Sebian Adi <sebian.lms@gmail.com>
2019-10-08 04:16:41 +0700

    cmd initial commit

--- wstest.py
+++
@@ -1,9 +0,0 @@
-from websocket import create_connection
-ws = create_connection("ws://api.warframestat.us/pc/cetusCycle")
-ws.send('"ShortString"')
-
-while True:
-  result =  ws.recv()
-  print ("Received '%s'" % result)
-
-ws.close()
